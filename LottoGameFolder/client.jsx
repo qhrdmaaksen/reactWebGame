a5f525d12a24388f91836c247da9169fa9df33a8
vitamin777 <rlaalsdn8@gmail.com>
2022-03-16 00:21:27 +0900

    lotto Hooks component content add commit

--- LottoGameFolder/client.jsx
+++ LottoGameFolder/client.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import LottoClassFunc from './lottoGameClass'
+//import LottoClassFunc from './lottoGameClass'
+import LottoClassFunc from './lottoGameHooks'
 
 ReactDOM.render(<LottoClassFunc />, document.querySelector('#root'))
