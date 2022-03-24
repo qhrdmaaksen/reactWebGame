b4986df9b65c256f6d1e49abca050277f73ca40c
vitamin777 <rlaalsdn8@gmail.com>
2022-03-24 23:59:44 +0900

    틱택톡 게임 내용 추가 , 및 각종 확장 추가

--- LottoGameFolder/lottoGameHooks.jsx
+++ LottoGameFolder/lottoGameHooks.jsx
@@ -1,66 +1,71 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
 import Ball from './Ball';
 
 function getWinNumbers() {
-  console.log('getWinNumber');
-  const candidate = Array(45).fill().map((v, i) => i + 1)
-  const shuffle = []
+  console.log('getWinNumbers');
+  const candidate = Array(45).fill().map((v, i) => i + 1);
+  const shuffle = [];
   while (candidate.length > 0) {
-    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0])
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
   }
-  const bonusNumber = shuffle[shuffle.length - 1]
-  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c)
-  return [...winNumbers, bonusNumber]
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
 }
 
-const lottoHooks = () => {
-  const [winNumbers, setWinNumbers] = useState(getWinNumbers())
-  const [winBalls, setWinBalls] = useState([])
-  const [bonus, setBonus] = useState(null)
-  const [redo, setRedo] = useState(false)
-  const timeouts = useRef([])
+const Lotto = () => {
+  const lottoNumbers = useMemo(() => getWinNumbers(), []); // useMemo 는 값을 저장한다 input [] 이 바뀌기 전까지
+  const [winNumbers, setWinNumbers] = useState(lottoNumbers);
+  const [winBalls, setWinBalls] = useState([]);
+  const [bonus, setBonus] = useState(null);
+  const [redo, setRedo] = useState(false);
+  const timeouts = useRef([]);
 
-  useEffect = (() => {
+  useEffect(() => {
     console.log('useEffect');
     for (let i = 0; i < winNumbers.length - 1; i++) {
       timeouts.current[i] = setTimeout(() => {
-        setWinBalls((prevBalls) => [...prevBalls, winNumbers[i]])
-      }, (i + 1) * 1000)
+        setWinBalls((prevBalls) => [...prevBalls, winNumbers[i]]);
+      }, (i + 1) * 1000);
     }
     timeouts.current[6] = setTimeout(() => {
-      setBonus(winNumbers[6])
-      setRedo(true)
-    }, 7000)
+      setBonus(winNumbers[6]);
+      setRedo(true);
+    }, 7000);
     return () => {
       timeouts.current.forEach((v) => {
-        clearTimeout(v)
-      })
-    }
-  }, [timeouts.current]) // input 이 빈 배열이면 componentDidMount 랑 똑같다
-  // 배열에 요소가 있다면 componentDidMount && componentDidUpdate 둘 다 수행
-  // 조건문이 들어갈 수 있다
-  // 바뀔때 작동
+        clearTimeout(v);
+      });
+    };
+  }, [timeouts.current]); // useEffect 는 로직을 실행하는데 input [] 이 바뀔때 실행
+  // 빈 배열이면 componentDidMount와 동일
+  // 배열에 요소가 있으면 componentDidMount랑 componentDidUpdate 둘 다 수행
 
-  const onClickRedo = () => {
+  useEffect(() => {
+    console.log('로또 숫자를 생성합니다.');
+  }, [winNumbers]);
+
+  const onClickRedo = useCallback(() => { // useCallback 은 함수를 기억한다. input [winNumbers] 가 바뀌기 전까지만
     console.log('onClickRedo');
-    setWinNumbers(getWinNumbers())
-    setWinBalls([])
-    setBonus(null)
-    setRedo(false)
-    timeouts.current = []
-  }
+    console.log(winNumbers);
+    setWinNumbers(getWinNumbers());
+    setWinBalls([]);
+    setBonus(null);
+    setRedo(false);
+    timeouts.current = [];
+  }, [winNumbers]);
 
   return (
-    <>
-      <div>당첨 숫자</div>
-      <div id="결과창">
-        {winBalls.map((v) => <Ball key={v} number={v} />)}
-      </div>
-      <div>보너스!</div>
-      {bonus && <Ball number={bonus} />}
-      {redo && <button onClick={this.onClickRedo}>한 번 더 !</button>}
-    </>
-  )
-}
+      <>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => <Ball key={v} number={v} />)}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} onClick={onClickRedo} />}
+        {redo && <button onClick={onClickRedo}>한 번 더!</button>}
+      </>
+  );
+};
 
-export default lottoHooks ;
+export default Lotto;
